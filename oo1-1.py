b7e4e86d8ca55c3b5e657246d390e26c031d84bf
Ben Chen <idben.chen@gmail.com>
2026-07-13 10:30:03 +0800

    有意義的名稱

--- oo1-1.py
+++ oo1-1.py
@@ -1,9 +1,9 @@
-def remove_fainted_party_member(party_hp: list[int]) -> list[int]:
-    h: list[int] = []
-    for p in party_hp:
-        if p > 0:
-            h.append(p)
-    return h
+def remove_fainted_party_member(party_hp):
+    active_hps = []
+    for hp in party_hp:
+        if hp > 0:
+            active_hps.append(hp)
+    return active_hps
 
 members_hp = [100, 60, 0]
 result = remove_fainted_party_member(members_hp)
