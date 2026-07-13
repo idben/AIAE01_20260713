e72fd957037375771d168a58faeb497c2ff21524
Ben Chen <idben.chen@gmail.com>
2026-07-13 09:28:11 +0800

    function return

--- def1.py
+++ def1.py
@@ -2,4 +2,13 @@ def welcome():
     print("歡迎來到 Python 程式教室！")
     print("讓我們開始學習函式吧。")
 
+def calc_area(width, height):
+    area = width * height
+    return area
+
+# 使用前需要先定義
 welcome()
+area1 = calc_area(5, 10)
+# function 沒有寫 return，預設值是 None
+# function 沒有 return，無法把結果傳出給外部使用
+print(f"面積是 {area1} 平方公分")
