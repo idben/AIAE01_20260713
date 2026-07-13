cfe9dbca6ac135f286acd3e5b935e25bdee042ad
Ben Chen <idben.chen@gmail.com>
2026-07-13 13:52:28 +0800

    以簡易購物車為情境

--- oo2-2.py
+++ oo2-2.py
@@ -4,7 +4,7 @@ class PartyMember:
 
     # 設定類別專屬方法
     # 第一個參數通常是物件自己, 會用 self 來代替
-    def take_damage(self, damage: int):
+    def take_damage(self, damage: int) -> None:
         print(f"{self.name} 角色受到的傷害是: {damage}")
         self.hp -= damage
         
@@ -18,3 +18,31 @@ hero.take_damage(10)
 man2.take_damage(1)
 print(f"{hero.name} 角色的生命值是: {hero.hp}")
 print(f"{man2.name} 角色的生命值是: {man2.hp}")
+
+print("-"*30)
+
+# 以簡易購物車為情境
+class Cart:
+    # 屬性
+    ip = "172.22.33.56"
+    total_money = 0
+
+    # -> None 回傳型別是 None = 沒有回傳
+    def add_to_cart(self, money: int) -> None:
+        print(f"  來自 {self.ip}，加入購物車 {money} 元")
+        self.total_money += money
+
+m_no1 = Cart()
+m_no1.ip = "172.22.33.1"
+m_no2 = Cart()
+m_no2.ip = "172.22.33.2"
+m_no1.add_to_cart(50)
+m_no1.add_to_cart(90)
+m_no1.add_to_cart(1250)
+m_no2.add_to_cart(648)
+m_no2.add_to_cart(1350)
+m_no2.add_to_cart(652)
+
+
+print(f"來自 {m_no1.ip}，消費金額是 {m_no1.total_money} 元")
+print(f"來自 {m_no2.ip}，消費金額是 {m_no2.total_money} 元")
